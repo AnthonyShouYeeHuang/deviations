9c48cbaa20b17cb7956d1398f6e2c139ee545aff
AnthonyShouYeeHuang <anthuang@outlook.com>
2025-10-25 21:08:00 -0500

    bug fix

---
+++ apps/web/src/vite-env.d.ts
@@ -0,0 +1,9 @@
+﻿/// <reference types="vite/client" />
+
+// (Optional) If you want strong typing for your custom vars:
+interface ImportMetaEnv {
+  readonly VITE_API_URL?: string;
+}
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
